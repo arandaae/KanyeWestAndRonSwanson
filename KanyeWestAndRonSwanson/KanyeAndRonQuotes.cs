a379e1edb0c7507179e2b46c2de1cb229bb8f644
Armando Aranda <arandaae19@gmail.com>
2022-05-25 12:43:57 -0500

    first commit

---
+++ KanyeWestAndRonSwanson/KanyeAndRonQuotes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWestAndRonSwanson
+{
+    public class KanyeAndRonQuotes
+    {
+        public HttpClient _client;
+
+        public KanyeAndRonQuotes(HttpClient client)
+        {
+            _client = new HttpClient();
+        }
+
+        public string KanyeWest()
+        {
+            //Kanye Quote
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            //Ron Quote
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+        }
+    }
+}
---
+++ KanyeWestAndRonSwanson/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWestAndRonSwanson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new KanyeAndRonQuotes(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye West: {quote.KanyeWest()}");
+                Console.WriteLine();
+                Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
